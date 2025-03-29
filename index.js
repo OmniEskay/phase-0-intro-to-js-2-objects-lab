233d522b153debede03afcbb960d6d2ab547a81b
+ <omnieskay@proton.me>
2025-03-29 16:25:02 +0300

    add objects to index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+let employee = {
+    name: 'Jane Smith',
+    streetAddress: '456 Oak Avenue'
+  };
+
+  function updateEmployeeWithKeyAndValue(employeeObj, key, value) {
+    const newEmployee = {
+      ...employeeObj,
+      [key]: value 
+    };
+    return newEmployee;
+  }
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employeeObj, key, value) {
+    employeeObj[key] = value;
+    return employeeObj;
+  }
+
+  function deleteFromEmployeeByKey(employeeObj, key) {
+    const { [key]: removedValue, ...newEmployee } = employeeObj;
+    return newEmployee;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employeeObj, key) {
+    delete employeeObj[key];
+    return employeeObj;
+  }
+  
